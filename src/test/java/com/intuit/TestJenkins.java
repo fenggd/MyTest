4d2e72fbeb8a889ea0cfd893ed9b514e67b9bb02
Cynthia Geng <cgeng@intuit.com>
2016-06-29 11:32:56 +0800

    update test

--- src/test/java/com/intuit/TestJenkins.java
+++ src/test/java/com/intuit/TestJenkins.java
@@ -7,9 +7,10 @@ import org.testng.annotations.Test;
 public class TestJenkins {
 
     @Test
-    public void testJenkins(){
+    public void testJenkins() throws InterruptedException{
         WebDriver driver = new FirefoxDriver();
         driver.get("https://www.baidu.com/");
+        Thread.sleep(10000);
         System.out.println("test1");
     }
 }
