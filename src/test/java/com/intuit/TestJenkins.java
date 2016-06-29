198eb2569cda8f930a5be0f5019c331f673b1b09
Cynthia Geng <cgeng@intuit.com>
2016-06-29 14:39:59 +0800

    update

--- src/test/java/com/intuit/TestJenkins.java
+++ src/test/java/com/intuit/TestJenkins.java
@@ -1,14 +1,16 @@
 package com.intuit;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.Test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
 
 public class TestJenkins {
 
     @Test
-    public void testJenkins() throws InterruptedException{
-        WebDriver driver = new FirefoxDriver();
+    public void testJenkins() throws InterruptedException {
+        DesiredCapabilities dc = DesiredCapabilities.firefox();
+        WebDriver driver = new RemoteWebDriver(dc);
         driver.get("https://www.baidu.com/");
         Thread.sleep(10000);
         driver.quit();
